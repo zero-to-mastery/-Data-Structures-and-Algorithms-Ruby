bda2242f40d322e2b02c07ecec1ec1eb03109f13
Juan Rodriguez <jrcam95@gmail.com>
2021-10-11 15:24:01 -0400

    updated append and prepend functions

--- data-structures/linked-lists/implementation.rb
+++ data-structures/linked-lists/implementation.rb
@@ -12,6 +12,14 @@
 #   }
 # };
 
+class Node
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
+
+
 class LinkedList
   def initialize(value)
     @head = {
@@ -23,20 +31,14 @@ class LinkedList
   end
 
   def append(value)
-    new_node = {
-      value: value,
-      next: nil
-    }
+    new_node = Node.new(value)
     @tail[:next] = new_node
     @tail = new_node
     @length += 1
   end
 
   def prepend(value)
-    new_node = {
-      value: value,
-      next: nil
-    }
+    new_node = Node.new(value)
     new_node[:next] = @head
     @head = new_node
     @length += 1
