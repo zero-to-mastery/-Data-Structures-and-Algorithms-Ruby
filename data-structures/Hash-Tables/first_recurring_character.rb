010b1e4bebf5aa76dab6059dbddf946bf273ba75
Juan Rodriguez <jrcam95@gmail.com>
2021-11-26 22:42:43 -0500

    Refactored first_recurring character problem for hash tables

--- data-structures/Hash-Tables/first_recurring_character.rb
+++ data-structures/Hash-Tables/first_recurring_character.rb
@@ -7,21 +7,8 @@
 # Given and array =[2,3,4,5]
 #It should return undefined
 
-# This is my original answer
-def first_recurring_character(input)
-  map = Hash[array.collect{|item| [item, 0 ]}]
-  input.each do |value|
-    map[value] += 1
-    if map[value] == 2
-      return value
-    end
-  end
-  return nil
-end
-
-
 # This is Andrei's answer
-def first_recurring_character2(input)
+def first_recurring_character(input)
   map = {}
   input.each do |value|
     if(map[value] != nil)
@@ -34,10 +21,10 @@ def first_recurring_character2(input)
 end
 
 # case 1.)
-p first_recurring_character2([2,5,1,2,3,5,1,2,4])
+p first_recurring_character([2,5,1,2,3,5,1,2,4])
 
 # case 2.)
-p first_recurring_character2([2,1,1,2,3,5,1,2,4])
+p first_recurring_character([2,1,1,2,3,5,1,2,4])
 
 # case 3.)
-p first_recurring_character2([2,3,4,5])
+p first_recurring_character([2,3,4,5])
